5eade3260470c3f2ae69b1e81459b198cd909970
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-31 22:42:09 -0700

    implemented invertBinaryTree.ts

---
+++ invertBinaryTree.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+	val: number;
+	left: TreeNode | null;
+	right: TreeNode | null;
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.left = left === undefined ? null : left;
+		this.right = right === undefined ? null : right;
+	}
+}
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+	if (!root) return null;
+	if (!root.left && !root.right) return root;
+
+	let temp = root.left;
+	root.left = root.right;
+	root.right = temp;
+
+	if (root.left) invertTree(root.left);
+	if (root.right) invertTree(root.right);
+
+	return root;
+}
